d3c5860187220dcd4172dcabe6fe7c52d87812b5
Yehansa <insarid7@gmail.com>
2026-02-10 01:20:51 +0000

    finished util.h file

--- util.h
+++ util.h
@@ -157,12 +157,14 @@ int send_until_success(int fd, uint8_t* buffer, size_t len) {
     ssize_t status = 0, bytes_sent = 0;
     while (!ctrl_c_pressed) {
         // TODO: Send data to the specified file descriptor
-
+        status = send(fd, buffer + bytes_sent, len - bytes_sent, 0);
         if (status < 0) {
             if (errno == EAGAIN || errno == EWOULDBLOCK) {
                 // TODO: Handle the case where the operation would block
                 //       (errno is EAGAIN or EWOULDBLOCK)
                 // Hint: Just sleep for a bit and try again
+                sleep(1);
+                continue;
             } 
             else {
                 // If errno is anything else, return -1
@@ -171,6 +173,14 @@ int send_until_success(int fd, uint8_t* buffer, size_t len) {
         } else {
             // TODO: If the data is successfully sent, update the number of bytes sent
             //       return 0 if all data has been sent
+            if (status == 0) {
+                return -1;
+            } else {
+                bytes_sent += status;
+                if (bytes_sent >= len) {
+                    return 0;
+                }
+            }
         }
     }
     // Return 1 if the loop is exited
